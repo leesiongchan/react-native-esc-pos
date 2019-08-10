4a6efaabede37d523b2def63c1b16b9b3b02b68b
Cody <yuchuen90@hotmail.com>
2019-08-10 15:45:53 +0800

    Fix missing import
    add DeviceEventEmitter that is missing

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-import { NativeModules } from 'react-native';
+import { DeviceEventEmitter, NativeModules } from 'react-native';
 
 const { LayoutBuilder } = NativeModules;
 
 const EscPos = {
   ...NativeModules.EscPos,
+  addListener = (eventName, cb) => {
+    switch (eventName) {
+      case 'bluetoothStateChanged':
+        EscPos.initBluetoothConnectionListener();
+        DeviceEventEmitter.addListener('bluetoothStateChanged', cb);
+        break;
+
+      default:
+        throw new Error('Event name not exist!');
+    }
+  },
   // TODO: What is the best way to add optional arguments to @ReactMethod? overloading doesn seem to be working??
   connect(address, port) {
     if (!port) {
@@ -14,18 +25,5 @@ const EscPos = {
   },
 };
 
-EscPos.addListener = (eventName, cb) => {
-    switch (eventName) {
-      case 'bluetoothStateChanged':
-        EscPos.initBluetoothConnectionListener();
-        DeviceEventEmitter.addListener('bluetoothStateChanged', cb);
-        break;
-  
-      default:
-        throw new Error('Event name not exist!');
-    }
-  };
-  
-      default:
 export { EscPos, LayoutBuilder };
 export default EscPos;
