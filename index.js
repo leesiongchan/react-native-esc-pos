54280d8d04b92db715fb3bf02b7b2bfbce73c6fe
Cody <yuchuen90@hotmail.com>
2019-08-10 12:33:31 +0800

    Add Bluetooth Connection Event Callback
    add bluetooth connection state callback with device information

--- index.js
+++ index.js
@@ -14,5 +14,17 @@ const EscPos = {
   },
 };
 
+EscPos.addListener = (eventName, cb) => {
+    switch (eventName) {
+      case 'bluetoothStateChanged':
+        EscPos.initBluetoothConnectionListener();
+        DeviceEventEmitter.addListener('bluetoothStateChanged', cb);
+        break;
+  
+      default:
+        throw new Error('Event name not exist!');
+    }
+  };
+  
 export { EscPos, LayoutBuilder };
 export default EscPos;
